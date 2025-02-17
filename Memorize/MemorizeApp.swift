3f1b85c40063387f654816768acbc29040bbb9bc
White Rabbit-CS95 <ioana.samuila@s.utm.ro>
2025-02-17 21:16:49 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Camelia Samuila on 16.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
